e6f9a794efb4532b890659810cf24637fd428512
Vanessa Valencia-Garcia <vanessa_vg@outlook.fr>
2025-02-19 15:16:04 +0100

    Etapes du MVP

---
+++ game.js
@@ -0,0 +1,108 @@
+import {quiz_culture_g} from './questions.js';
+
+// Variable pour suivre l'état du quiz
+let  currentQuestionIndex = 0;
+let answerSelected = false;
+let score = 0;
+
+// Récupérer les emplacements pour injecter la question,les options et le bouton Suivant
+const questionContainer = document.getElementById('question-text');
+const optionsContainer = document.getElementById('options-container');
+const nextButton = document.getElementById('next-button');
+const replayButton = document.getElementById('replay-button');
+
+// Fonction pour afficher une question basée sur l'index actuel
+function loadQuestion() {
+    // Vider le conteneur des options
+    optionsContainer.innerHTML = '';
+    // Réinitialisation
+    answerSelected = false;
+
+// Récupérer la question actuelle
+const currentQuestion = quiz_culture_g.questions[currentQuestionIndex];
+
+// Injecter le texte de la question dans l'emplacement dédié
+questionContainer.innerText = currentQuestion.text;
+
+// Pour chaque option, créer un bouton et l'ajouter au conteneur PUIS Injecter les options dans le HTML
+currentQuestion.options.forEach(option => {
+    const button = document.createElement('button');
+    button.innerText = option;
+    button.classList.add('option-button');
+    optionsContainer.appendChild(button);
+
+// Ajoute l'événement pour vérifier la réponse
+    button.addEventListener('click', () => checkAnswer(button, currentQuestion.correct_answer));
+    optionsContainer.appendChild(button);
+
+});
+// Désactiver le bouton Suivant au début
+    nextButton.disabled = true;
+}
+
+// Ajoute un écouteur d'événement pour le bouton "Suivant"
+
+nextButton.addEventListener('click' , () => {
+    // Incrémenter l'index de la question
+    currentQuestionIndex++;
+    
+    // Vérifier s'il reste des questions
+    if (currentQuestionIndex < quiz_culture_g.questions.length) {
+        // Afficher la question suivante
+        loadQuestion();
+    } else {
+        // Si plus de questions, indiquer la fin du quiz
+        questionContainer.innerText = `Quiz terminé ! 🎉 Score final: ${score}/${quiz_culture_g.questions.length}`;
+        let message = '';
+        if (score === quiz_culture_g.questions.length) {
+            message = 'Bravo ! Parfait 🎯';
+        } else if (score > quiz_culture_g.questions.length / 2) {
+            message = 'Bien joué ! 👍';
+        } else {
+            message = 'Peut mieux faire 💪';
+        }
+        optionsContainer.innerText = ''; // Effacer les options
+        nextButton.style.display = 'none'; // Cacher le bouton Suivant
+        replayButton.style.display = 'inline-block'; // Afficher le bouton Rejouer
+    }
+
+});
+
+// Charger la première question au chargement de la page
+loadQuestion();
+
+replayButton.addEventListener('click' , () => {
+    // Réinitialiser l'index
+    currentQuestionIndex = 0;
+    score = 0;
+
+    // Cacher le bouton rejouer et afficher le bouton Suivant
+    replayButton.style.display = 'none';
+    nextButton.style.display = 'inline-block'
+
+    // Recharger la première question
+    loadQuestion();
+})
+
+// Ajout de la fonction checkAnswer
+function checkAnswer(button, correct_answer) {
+    if (answerSelected) return; // Empeche de cliquer plusieurs fois
+    answerSelected = true;
+
+    const allButtons = document.querySelectorAll('.option-button');
+
+    // Désactive tous les boutons pour éviter plusieurs clics
+    allButtons.forEach(btn => btn.disabled = true);
+
+    if (button.innerText.trim() === correct_answer.trim()) {
+        button.style.border = '3px solid green';
+        score++;
+    } else {
+        button.style.border = '3px solid red';
+    }
+    //Affiche le score après chaque question (optionnel)
+    console.log(`Score actuel: ${score}`);
+
+    // Activer le bouton Suivant
+    nextButton.disabled = false;
+}
---
+++ questions.js
@@ -0,0 +1,122 @@
+export const quiz_culture_g = {
+    questions: [
+        {
+            text: "Quel est le rôle des sourcils ?",
+            options: [
+                "A - Les faire froncer quand on est en colère",
+                "B - Empêcher la transpiration de tomber dans les yeux",
+                "C - Protéger les yeux des rayons UV"
+            ],
+            correct_answer: "B - Empêcher la transpiration de tomber dans les yeux",
+            explanation: "Les sourcils ont la fonction de diriger la sueur et l'humidité loin des yeux !"
+        },
+
+        {
+            text: "Quel animal peut frapper si fort qu'il fait bouillir l'eau autour de lui ?",
+            options: [
+                "A - Le Poisson-globe",
+                "B - La Crevette-mante",
+                "C - Le Calamart Géant",
+                "D - Le Poisson-lune",
+            ],
+            correct_answer: "B - La Crevette-mante",
+            explanation: "Elle frappe avec une telle vitesse que cela crée une bulle de cavitation, ce qui chauffe l'eau à des températures extrêmes.",
+        },
+
+        {
+            text: "Quel joueur est surnommé CR7 ?",
+            options: [
+                "A - Lionel Messi",
+                "B - Carlos Rodriguez",
+                "C - Cristiano Ronaldo",
+                "D - Kylian Mbappé",
+            ],
+            correct_answer: " C - Cristiano Ronaldo" ,
+            explanation: "CR7 est une légende du football, connu pour son numéro 7 et ses exploits avec le Real Madrid et le Portugal.",
+        },
+
+        {
+            text: "Quel est le pays qui consomme le plus de chocolat ?",
+            options: [
+                "A - La Suisse",
+                "B - Les Etats-Unis",
+                "Le Japon",
+                "Le Canada",
+            ],
+            correct_answer: "A - La Suisse",
+            explanation: "La Suisse est réputée pour sa consommation massive de chocolat, et ses habitants sont parmi les plus grands amateurs de chocolat au monde !",
+        },
+
+        {
+            text: "Quel animal peut dormir jusqu'à 3 ans si les conditions ne sont pas idéales ?",
+            options: [
+                "A - La grenouille",
+                "B - L'escargot",
+                "C - L'ours ",
+                "D - Le serpent ",
+            ],
+            correct_answer: "B - L'escargot",
+            explanation: "Les escargots peuvent entrer en hibernation ou en estivation pendant plusieurs années en fonction des conditions environnementales.",
+        },
+
+        {
+            text: "Qui est la joueuse la plus titrée au Ballon d'Or Féminin ?",
+            options: [
+                "A - Alexia Putellas",
+                "B - Marta",
+                "C - Megan Rapinoe",
+                "D - Ada Hegerberg",
+            ],
+            correct_answer: "A - Alexia Putellas",
+            explanation: "Alexia Putellas a remporté deux Ballons d'Or consécutifs en 2021 et 2022 avec le FC Barcelone.",
+        },
+
+        {
+            text: 'Quel aliment peut "exploser" si tu le mets dans le micro-ondes sans le percer ?',
+            options: [
+                "A - Un Oeuf",
+                "B - Du Chocolat",
+                "C - Une Tomate",
+                "D - Une Pomme de Terre",
+            ],
+            correct_answer: "A - Un Oeuf",
+            explanation: "Si tu mets un œuf entier dans le micro-ondes sans le percer, la vapeur s’accumule et l'œuf peut éclater, créant un joli désastre.",
+        },
+
+        {
+            text: "Quel animal dort en se tenant la main avec ses congenères",
+            options: [
+                "A  - Le Dauphin",
+                "B - L'Otarie",
+                "C - La Loutre de Mer",
+                "D - Le Castor",
+            ],
+            correct_answer: "C - La Loutre de Mer",
+            explanation: "Les loutres de mer se tiennent la main pendant leur sommeil pour ne pas dériver avec le courant.",
+        },
+
+        {
+            text: "Quelle nation a remporté la Coupe du Monde de Football 2018",
+            options: [
+                "A - Brésil",
+                "B - France",
+                "C - Argentine",
+                "D - Portugal",
+            ],
+            correct_answer: "B - France",
+            explanation: "La France a gagné la Coupe du Monde 2018 en battant la Croatie 4-2 en finale.",
+        },
+
+        {
+            text: "A quelle heure peut-on commencer l'apéro ?",
+            options: [
+                "A - 18H",
+                "B - 19H",
+                "C - 10H",
+                "D - Quand on veut !",
+            ],
+            correct_answer: "D - Quand on veut !",
+        },
+
+    ]
+}
